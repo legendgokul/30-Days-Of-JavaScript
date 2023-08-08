867eb260c7faa02100e4b023b18e08ce237f41ee
Gokul <iamgokulnathanmcs@gmail.com>
2023-08-08 20:31:31 +0530

    pushed 5, 6, 7

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,152 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+
+console.log("Countries",countries);
+
+let test = true;
+console.log(Array.isArray(countries));
+
+
+const numbers = [1,2,3,4,5]
+
+console.log(numbers.slice()) // -> it copies all item , meaning its wont slice anything.
+console.log(numbers.slice(4)) // -> if one parameter is it is considered as start point and enpoint is fixed to length by default.
+console.log(numbers.slice(0, numbers.length)) // it copies all  item
+console.log(numbers.slice(1,4)) // -> [2,3,4] // it doesn't include the ending position
+
+//Splice
+console.log("Splice");
+// Splice can take 3 different parameter 
+// 1.starting index (0 -> n-1)
+// 2.no of items to be removed
+// 3.array of item to be added  // this will be added in place of what element is being removed.
+// Splice will return items which are removed.
+// and the original array will contain remaining data.
+const number = [1,2,3,4,5,6,7];
+
+var data  = number.splice(0,4);  // data 1,2,3,4
+console.log("data"+data);    
+console.log("number"+number);    // number 5,6,7
+
+var spliceResult = number.splice(1,1,8,9,10);
+console.log(spliceResult);  // 6
+console.log(number); // 5,8,9,10,7
+
+console.log("learning shift and unshift");
+var Shiftnum = [1,2,3,4,5,6,7];
+Shiftnum.shift();
+console.log(Shiftnum); // removes one number from the start of list , it also returns the items which is getting removed during shift operation.
+
+Shiftnum.unshift(1)
+console.log(Shiftnum); // Added the input item at the start and returns the new length as output.
+Shiftnum.unshift(2,3) 
+console.log(Shiftnum); // we can add an array of new items also at the start.
+Shiftnum.unshift()
+console.log(Shiftnum); // if we dont provide any value for unshift , there wont be any change made to the list and length of array will be returned.
+
+
+
+var BooleanList = [true,false,false,true,true];
+BooleanList.sort();
+console.log(BooleanList); // doing sort of boolean will rearrange false to left and true to right.
+
+
+var BooleanList = [true,"false",false,"true",'',undefined];
+BooleanList.sort();
+console.log(BooleanList); // ['', 'false', false, true, 'true', undefined]
+
+
+console.log("learning multi D array");
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const relationalDB = ['postgres','Sql'];
+const nonrelationalDB = ['mongo'];
+const database = [relationalDB,nonrelationalDB]
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = [frontEnd, backEnd , database]
+
+console.log(fullStack);
+console.log(fullStack[2][0]); //['postgres', 'Sql']
+
+fullStack.unshift(['css','bootstrap']); 
+fullStack[0].push("tailwind");
+console.log(fullStack[0].push("tailwind")); 
+console.log(fullStack[0].pop()); 
+
+const arr = Array(8);
+console.log(arr[0]);
+
+let s1 = "momomomomom";
+console.log(s1.split('m')); 
+console.log(s1.length);
+
+
+console.log("###################### Exercise #####################");
+//1
+var emptyArray =  Array(8);
+
+//2
+var arritem = [1,2,3,4,5,6,7];
+
+//3
+var length = arritem.length;
+
+//4
+var i = arritem[0];
+console.log(i);
+console.log(arritem[length-1]);
+console.log(arritem[(length-1) /2]);
+
+//5
+var mixarr = [1,'2',"name",true,undefined,{name:"Gokul"}];
+console.log(mixarr);
+
+//6 
+var itCompanies = ["Facebook","Google","Microsoft","Apple","IBM","Oracle","Amazon"];
+console.log(itCompanies);
+
+//8
+console.log("Companies length :"+itCompanies.length);
+
+//9
+console.log(itCompanies[i]);
+console.log(itCompanies[length-1]);
+console.log(itCompanies[(length-1) /2]);
+
+//10
+console.log("printing each company");
+itCompanies.forEach(x=> console.log(x));
+console.log(itCompanies.toString() + " are big IT companies" );
+
+
+//slice first 3
+console.log(itCompanies.slice(0,3));
+
+//last 3
+var len = itCompanies.length;
+console.log(itCompanies.slice(len-3,len));
+itCompanies.shift();
+var remaining  = itCompanies.splice(0,1);
+console.log(itCompanies);
+
+//remove all
+
+itCompanies.splice(0);
+
+//Exercise 3
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+console.log(ages.sort());
+
+function FindMedian (){
+    Math.median(ages);
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,294 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+  
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node'];
+  console.log("Default FOR :");
+  for(let i=0;i<countries.length;i++){
+      console.log(countries[i]);
+  }
+  
+  console.log("Default While :");
+  let index = 0;
+  while (index<countries.length){
+      console.log(countries[index++]);
+  }
+  
+  console.log("Default Do-While :");
+  index = 0;
+  do{
+      console.log(countries[index++]);
+  }while(index<countries.length);
+  
+  // printing patern :
+  
+  /*
+        #
+       ##
+      ###
+     ####
+    #####
+   ######
+  */
+  console.log("right Pyramid :");
+  let pattern = "";
+  let len = 7;
+  for( let i=1; i<=len; i++ )
+  {
+      //logic to add space
+      for(let j =1;j<=len-i;j++){
+          pattern += " ";
+      }
+      //logic to add #
+      for(let j =1;j<=i;j++){
+          pattern += "#";
+      }
+      //finally ad next line.
+      pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  #      
+  ##     
+  ###    
+  ####   
+  #####  
+  ###### 
+  #######
+  */
+  console.log("left Pyramid :");
+   pattern = "";
+   len = 7;
+  for( let i=1; i<=len; i++ )
+  {
+      //logic to add #
+      for(let j =1;j<=i;j++){
+          pattern += "#";
+      }
+      //finally ad next line.
+      pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  #######
+  ######
+  #####
+  ####
+  ###
+  ##
+  #
+  */
+  console.log("Reverse left Pyramid :");
+  pattern = "";
+  len = 7;
+  for( let i=0; i<len; i++ )
+  {
+     //logic to add #
+     for(let j=len-i; j>0  ;j--){
+         pattern += "#";
+     }
+     //finally ad next line.
+     pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  #######
+   ######
+    #####
+     ####
+      ###
+       ##
+        #
+  */
+  console.log("reverse right Pyramid :");
+  pattern = "";
+  len = 7;
+  for( let i=0; i<len; i++ )
+  {
+      for(let j=i; j>0  ;j--){
+          pattern += " ";
+      }
+     //logic to add #
+      for(let j=len-i; j>0  ;j--){
+          pattern += "#";
+      }
+     //finally ad next line.
+     pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  Hallow left pyramid
+  
+  *
+  **
+  * *
+  *  *
+  *   *
+  ******
+  */
+  console.log("Hallow Left Pyramid :");
+  pattern = "";
+  len = 7;
+  for( let i=1; i<len; i++ )
+  {
+      for( let j=0; j<i ;j++ ){
+          if( j == 0 || j == i-1 || i == len-1 ){
+              pattern += "#";
+          }else{
+              pattern += " ";
+          } 
+      }
+     //finally ad next line.
+     pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  Pyramid
+  
+  ----#
+  ---###
+  --#####
+  -#######
+  */
+  console.log("Pyramid :");
+  pattern = "";
+  len = 5;
+  for( let i=1; i<len; i++ )
+  {
+      for( let j=1; j<len-i ;j++ ){
+          pattern +="-";
+      }
+      for( let j=0; j<(i*2)-1 ;j++ ){
+          pattern +="#";
+      }
+     //finally ad next line.
+     pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  Reverse pyramid
+  
+  #######
+  -#####
+  --###
+  ---#
+  */
+  console.log("Reverse pyramid :");
+  pattern = "";
+  len = 5;
+  for( let i=1; i<len; i++ )
+  {
+      for( let j=len-1; j>len-i ;j-- ){
+          pattern +="-";
+      }
+      for( let j=0; j<((len-i)*2)-1 ;j++ ){
+          pattern +="#";
+      }
+     //finally ad next line.
+     pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  /*
+  hallow pyramid
+  
+  ----#
+  ---# #
+  --#   #
+  -#     #
+  #########
+  */
+  console.log("hallow pyramid :");
+  pattern = "";
+  len = 5;
+  for( let i=1; i<=len; i++ )
+  {
+      for( let j=0; j<len-i ;j++ ){
+          pattern +="-";
+      }
+      for( let j=1; j<(i*2) ;j++ ){
+          if(j==1 || j==(i*2)-1 ||i == len ){
+              pattern +="#";
+          }else{
+              pattern +=" ";
+          }
+      }
+     //finally ad next line.
+     pattern +="\n";
+  }
+  console.log(pattern);
+  
+  
+  console.log("creating array of random numbers without repeat :");
+  
+  let nomnom = [];
+  for(let i=0;i<10;i++){
+      let newNum = Math.floor(Math.random()*100);
+     let flag  = nomnom.includes(newNum);
+      while(flag){
+          newNum = Math.floor(Math.random()*100);
+          console.log("damn");
+          flag  = nomnom.includes(newNum);
+      }
+      nomnom.push(newNum);
+  }
+  console.log(nomnom);
+  
+  
+  let nomnom1 = [];
+  while(nomnom1.length<10){
+      let newNum = Math.floor(Math.random()*100);
+      if(!nomnom1.includes(newNum)){
+          nomnom1.push(newNum);
+      }
+  }
+  console.log(nomnom1);
+  
+  
+  const webTechs = [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Redux',
+      'Node',
+      'MongoDB'
+    ];
+  
+  
+  function displayWebTech(webTech){
+      let Response  = [];
+  
+      for(let x of webTech){
+          let work1 = x.slice(0,2)
+          let work2 = x.slice(2,x.length)
+         Response.push([x,work1.toUpperCase()+work2.toLowerCase(),x.length])
+      }
+      console.log(Response.sort());
+  }
+  
+  displayWebTech(webTechs);
